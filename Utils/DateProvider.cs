16c68e6111764fedd2ac5bbc03be41618f5d803b
Dmitry Shestakov <6reduk@gmail.com>
2025-05-28 23:44:11 +0300

    Add date and guid provider

---
+++ Utils/DateProvider.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Pupo4ek.Utils;
+
+public class DateProvider : IDateProvider
+{
+    public DateTime Now()
+    {
+        return DateTime.Now;
+    }
+
+    public DateTime UtcNow()
+    {
+        return DateTime.UtcNow;
+    }
+
+    public DateTime Today()
+    {
+        return DateTime.Today;
+    }
+}
---
+++ Utils/IDateProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Pupo4ek.Utils;
+
+public interface IDateProvider
+{
+    public DateTime Now();
+    public DateTime UtcNow();
+    public DateTime Today();
+}
